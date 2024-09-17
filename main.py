90ad0204c46dd14d50acfd332117fccc994ad8e9
Arbitrum <33kkm33@gmail.com>
2024-09-18 00:14:22 +0900

    2

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import openpyxl
 import os
+import time
 
 if __name__ == "__main__":
     if os.path.exists("test.xlsx"):
@@ -10,3 +11,7 @@ if __name__ == "__main__":
         wb = openpyxl.Workbook()
         wb.save("test.xlsx")
         print("파일이 존재하지않아 생성하고 저장함")
+
+    while True:
+        time.sleep(1)
+        print("작동중", time.strftime("%H:%M:%S", time.localtime()))
